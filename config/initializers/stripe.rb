bf255886dba2a1b61ada8233f20da796782fd34f
VuHoangQuanNguyen <hoangquan2561999hl@gmail.com>
2020-04-23 17:56:15 -0500

    fix error 500

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,8 +1,15 @@
 # frozen_string_literal: true
 
+# Rails.configuration.stripe = {
+#   publishable_key: ENV['PUBLISHABLE_KEY'],
+#   secret_key: ENV['SECRET_KEY']
+# }
+
+# Stripe.api_key = Rails.configuration.stripe[:secret_key]
+
 Rails.configuration.stripe = {
-  publishable_key: ENV['PUBLISHABLE_KEY'],
-  secret_key: ENV['SECRET_KEY']
+  publishable_key: ENV['pk_test_hnRNkJwJxwIZ0Ln4WMAOCcJz00ZrCq78li'],
+  secret_key: ENV['sk_test_eZT4YztklI9vKCUL3O1yITg800r5FGUxG4']
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
