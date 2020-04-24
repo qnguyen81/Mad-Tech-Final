ff938d88c14e59d8e8208282d41bd1e2fe215d67
VuHoangQuanNguyen <hoangquan2561999hl@gmail.com>
2020-04-24 09:36:49 -0500

    stripe

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -8,8 +8,8 @@
 # Stripe.api_key = Rails.configuration.stripe[:secret_key]
 
 Rails.configuration.stripe = {
-  publishable_key: ENV['PUBLISHABLE_KEY'],
-  secret_key: ENV['SECRET_KEY']
+  publishable_key: 'pk_test_hnRNkJwJxwIZ0Ln4WMAOCcJz00ZrCq78li',
+  secret_key: 'sk_test_eZT4YztklI9vKCUL3O1yITg800r5FGUxG4'
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
