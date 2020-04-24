ee8ac506be9bb1cecfa2c0322108a368da6bd7a7
VuHoangQuanNguyen <hoangquan2561999hl@gmail.com>
2020-04-24 01:50:33 -0500

    fix seed with image

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require 'csv'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -17,12 +18,17 @@
 #   Province.create!(name: full)
 # end
 # puts "#{Province.count} are seeded"
-cgrs = Category.where('name == "Laptop"')
+cgrs = Category.create!(
+  name: 'Pc-Laptop',
+  description: 'High end pc-laptop'
+)
 
-laptop_data = File.read("#{Rails.root}/public/laptops.csv")
+laptop_data = File.read("#{Rails.root}/public/laptop.csv")
 laptops = CSV.parse(laptop_data, headers: true)
 
 laptops.each do |row|
-  # category.products.create(name: full.Product, price: full.Price_euros, description: full.Inches + full.TypeName + )
-  puts row['TypeName'] + ' ' + row['Inches'] + ' ' + row['ScreenResolution'] + ' ' + row['Cpu'] + ' ' + row['Ram'] + ' ' + row['Memory'] + ' ' + row['Gpu'] + ' ' + row['OpSys']
+  des = row['Title'] + ' ' + row['availabilityMessageSearchPickup_2eGze']
+  prod = cgrs.products.create(name: row['Title'], price: row['Price'], description: des)
+  downloaded_image = open(URI.escape((row['Thumbnail']).to_s))
+  prod.image.attach(io: downloaded_image, filename: "image-#{row['Title']}.jpg")
 end
