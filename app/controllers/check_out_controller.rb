8a7bcae5ceca1e6a6692bb0be0f926c5ace33b61
VuHoangQuanNguyen <hoangquan2561999hl@gmail.com>
2020-04-24 05:39:20 -0500

    debug 500

--- app/controllers/check_out_controller.rb
+++ app/controllers/check_out_controller.rb
@@ -1,108 +1,108 @@
-# frozen_string_literal: true
+# # frozen_string_literal: true
 
-class CheckOutController < ApplicationController
-  before_action :authenticate_customer!
-  def index
-    @customer = current_customer
-    @cart = session[:cart] if session[:cart].present?
-    @features = helpers.find_features
+# class CheckOutController < ApplicationController
+#   before_action :authenticate_customer!
+#   def index
+#     @customer = current_customer
+#     @cart = session[:cart] if session[:cart].present?
+#     @features = helpers.find_features
 
-    @province = Province.find(@customer.province_id)
+#     @province = Province.find(@customer.province_id)
 
-    @sub_total = helpers.calculate_subtotal
+#     @sub_total = helpers.calculate_subtotal
 
-    @gst = @province.gst
-    @gst_total = (@province.gst / 100) * @sub_total
-    @pst = @province.pst
-    @pst_total = (@province.pst / 100) * @sub_total
-    @hst = @province.hst
-    @hst_total = (@province.hst / 100) * @sub_total
-    @total_tax =  @hst_total + @gst_total + @pst_total
+#     @gst = @province.gst
+#     @gst_total = (@province.gst / 100) * @sub_total
+#     @pst = @province.pst
+#     @pst_total = (@province.pst / 100) * @sub_total
+#     @hst = @province.hst
+#     @hst_total = (@province.hst / 100) * @sub_total
+#     @total_tax =  @hst_total + @gst_total + @pst_total
 
-    @total = @total_tax + @sub_total
-  end
+#     @total = @total_tax + @sub_total
+#   end
 
-  def create
-    @total = params[:total].to_i
-    if @total.nil?
-      redirect_yo root_path
-      return
-    end
+#   def create
+#     @total = params[:total].to_i
+#     if @total.nil?
+#       redirect_yo root_path
+#       return
+#     end
 
-    @session = Stripe::Checkout::Session.create(
-      payment_method_types: ['card'],
-      line_items: [{
-        name: 'Total: ',
-        amount: @total * 100,
-        currency: 'cad',
-        quantity: 1
-      }],
-      success_url: check_out_success_url + '?session_id={CHECKOUT_SESSION_ID}',
-      cancel_url: check_out_cancel_url
-    )
+#     @session = Stripe::Checkout::Session.create(
+#       payment_method_types: ['card'],
+#       line_items: [{
+#         name: 'Total: ',
+#         amount: @total * 100,
+#         currency: 'cad',
+#         quantity: 1
+#       }],
+#       success_url: check_out_success_url + '?session_id={CHECKOUT_SESSION_ID}',
+#       cancel_url: check_out_cancel_url
+#     )
 
-    respond_to do |format|
-      format.js
-    end
-    @customer = current_customer
+#     respond_to do |format|
+#       format.js
+#     end
+#     @customer = current_customer
 
-    @cart = session[:cart] if session[:cart].present?
-    @product_ids = []
+#     @cart = session[:cart] if session[:cart].present?
+#     @product_ids = []
 
-    @cart&.each_key { |key| @product_ids.push(key) }
+#     @cart&.each_key { |key| @product_ids.push(key) }
 
-    @products = helpers.find_features
+#     @products = helpers.find_features
 
-    @province = Province.find(@customer.province_id)
+#     @province = Province.find(@customer.province_id)
 
-    @sub_total = helpers.calculate_subtotal
+#     @sub_total = helpers.calculate_subtotal
 
-    @gst = @province.gst
-    @gst_total = (@province.gst / 100) * @sub_total
-    @pst = @province.pst
-    @pst_total = (@province.pst / 100) * @sub_total
-    @hst = @province.hst
-    @hst_total = (@province.hst / 100) * @sub_total
-    @total_tax =  @hst_total + @gst_total + @pst_total
+#     @gst = @province.gst
+#     @gst_total = (@province.gst / 100) * @sub_total
+#     @pst = @province.pst
+#     @pst_total = (@province.pst / 100) * @sub_total
+#     @hst = @province.hst
+#     @hst_total = (@province.hst / 100) * @sub_total
+#     @total_tax =  @hst_total + @gst_total + @pst_total
 
-    @total1 = @sub_total + @total_tax
-  end
+#     @total1 = @sub_total + @total_tax
+#   end
 
-  def success
-    @session = Stripe::Checkout::Session.retrieve(params[:session_id])
-    @payment_intent = Stripe::PaymentIntent.retrieve(@session.payment_intent)
+#   def success
+#     @session = Stripe::Checkout::Session.retrieve(params[:session_id])
+#     @payment_intent = Stripe::PaymentIntent.retrieve(@session.payment_intent)
 
-    @customer = current_customer
+#     @customer = current_customer
 
-    @cart = session[:cart] if session[:cart].present?
-    @product_ids = []
+#     @cart = session[:cart] if session[:cart].present?
+#     @product_ids = []
 
-    @cart&.each_key { |key| @product_ids.push(key) }
+#     @cart&.each_key { |key| @product_ids.push(key) }
 
-    @products = helpers.find_features
+#     @products = helpers.find_features
 
-    @province = Province.find(@customer.province_id)
+#     @province = Province.find(@customer.province_id)
 
-    @sub_total = helpers.calculate_subtotal
+#     @sub_total = helpers.calculate_subtotal
 
-    @gst = @province.gst
-    @gst_total = (@province.gst / 100) * @sub_total
-    @pst = @province.pst
-    @pst_total = (@province.pst / 100) * @sub_total
-    @hst = @province.hst
-    @hst_total = (@province.hst / 100) * @sub_total
-    @total_tax =  @hst_total + @gst_total + @pst_total
+#     @gst = @province.gst
+#     @gst_total = (@province.gst / 100) * @sub_total
+#     @pst = @province.pst
+#     @pst_total = (@province.pst / 100) * @sub_total
+#     @hst = @province.hst
+#     @hst_total = (@province.hst / 100) * @sub_total
+#     @total_tax =  @hst_total + @gst_total + @pst_total
 
-    @total = @sub_total + @total_tax
+#     @total = @sub_total + @total_tax
 
-    @order = Order.create(status: 'Paid', amount: @total, shipping_address: @customer.address, gst: @gst,
-                          pst: @pst, hst: @hst, customer_id: @customer.id)
-    @products.each do |product|
-      OrderDetail.create(price: product.price, quantity: @cart[product.id.to_s], product_id: product.id, order_id: @order.id)
-    end
+#     @order = Order.create(status: 'Paid', amount: @total, shipping_address: @customer.address, gst: @gst,
+#                           pst: @pst, hst: @hst, customer_id: @customer.id)
+#     @products.each do |product|
+#       OrderDetail.create(price: product.price, quantity: @cart[product.id.to_s], product_id: product.id, order_id: @order.id)
+#     end
 
-    session.delete(:cart)
-  end
+#     session.delete(:cart)
+#   end
 
-  def cancel; end
-end
+#   def cancel; end
+# end
--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -7,10 +7,10 @@
 
 # Stripe.api_key = Rails.configuration.stripe[:secret_key]
 
-Rails.configuration.stripe = {
-  publishable_key: ENV['pk_test_hnRNkJwJxwIZ0Ln4WMAOCcJz00ZrCq78li'],
-  secret_key: ENV['sk_test_eZT4YztklI9vKCUL3O1yITg800r5FGUxG4']
-}
+# Rails.configuration.stripe = {
+#   publishable_key: ENV['pk_test_hnRNkJwJxwIZ0Ln4WMAOCcJz00ZrCq78li'],
+#   secret_key: ENV['sk_test_eZT4YztklI9vKCUL3O1yITg800r5FGUxG4']
+# }
 
-Stripe.api_key = Rails.configuration.stripe[:secret_key]
+# Stripe.api_key = Rails.configuration.stripe[:secret_key]
 # PUBLISHABLE_KEY = pk_test_hnRNkJwJxwIZ0Ln4WMAOCcJz00ZrCq78li SECRET_KEY= sk_test_eZT4YztklI9vKCUL3O1yITg800r5FGUxG4
