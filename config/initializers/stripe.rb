8944e37d23cccce45a20f41b874e4d063f00c4e6
VuHoangQuanNguyen <hoangquan2561999hl@gmail.com>
2020-04-24 06:43:47 -0500

    test debug deploy

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -8,9 +8,10 @@
 # Stripe.api_key = Rails.configuration.stripe[:secret_key]
 
 Rails.configuration.stripe = {
-  publishable_key: ENV['pk_test_hnRNkJwJxwIZ0Ln4WMAOCcJz00ZrCq78li'],
-  secret_key: ENV['sk_test_eZT4YztklI9vKCUL3O1yITg800r5FGUxG4']
+  publishable_key: ENV['PUBLISHABLE_KEY'],
+  secret_key: ENV['SECRET_KEY']
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
 # PUBLISHABLE_KEY = pk_test_hnRNkJwJxwIZ0Ln4WMAOCcJz00ZrCq78li SECRET_KEY= sk_test_eZT4YztklI9vKCUL3O1yITg800r5FGUxG4
+heroku config:set PUBLISHABLE_KEY=pk_test_hnRNkJwJxwIZ0Ln4WMAOCcJz00ZrCq78li SECRET_KEY=sk_test_eZT4YztklI9vKCUL3O1yITg800r5FGUxG4
